8e6b5b5ffcee4eec3122c365adda9f6fa360b2d3
JISHNU M <@jishnujanan>
2022-05-07 19:02:50 +0530

    Jishnu-First-Commit

---
+++ android/app/src/main/java/com/example/hosteler/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.hosteler;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/Colors/AppColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors
+{
+  static const bgColor=Colors.black;
+}
